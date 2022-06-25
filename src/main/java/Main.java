95645e6303fd78c4761c6bebffd55607ee8cc22d
Tenae <tenae@zipcodersmbp107.myfiosgateway.com>
2022-06-25 19:47:08 -0400

    please work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner in = new Scanner(System.in);
+        String userInput;
+
+        System.out.println("What is your name?");
+
+        userInput = in.nextLine();
+
+        if (userInput.equalsIgnoreCase("Alice")) {
+            System.out.println("Hi" + " " + userInput);
+        } else if (userInput.equalsIgnoreCase("Bob")) {
+            System.out.println("Hi" + " " + userInput);
+        } else {
+            System.out.println("Incorrect username");
+        }
 
     }
 }
